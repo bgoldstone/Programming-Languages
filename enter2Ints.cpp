54b9fe128467ba9d54ae625e61716cdcd0dbfd45
Benjamin Goldstone <bgoldstone1@gmail.com>
2023-02-02 13:07:01 -0500

    added enter2Ints.cpp

---
+++ enter2Ints.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    printf("Please enter integer a:");
+    cin << a;
+
+    printf("\nPlease enter integer b:");
+    scanf("%d", &b);
+
+    int *pointerA = &a;
+    int *pointerB = &b;
+
+    cout << "\n"
+         << *pointerA << " " << *pointerB << endl;
+    return 0;
+}
